72801f6861b453435c05882d0c086a9d40955873
Yuanhsi Chung <freshliver.cys@gmail.com>
2023-01-10 12:26:23 +0800

    Format ftl_config.[ch] and add document comments

--- Toshiba-8c8w/ftl_config.c
+++ Toshiba-8c8w/ftl_config.c
@@ -46,7 +46,6 @@
 //   - First draft
 //////////////////////////////////////////////////////////////////////////////////
 
-
 #include <assert.h>
 #include "xil_printf.h"
 #include "memory_map.h"
@@ -56,152 +55,196 @@
 unsigned int storageCapacity_L;
 T4REGS chCtlReg[USER_CHANNELS];
 
+/**
+ * @brief The entry function for FTL initialization.
+ */
 void InitFTL()
 {
-	CheckConfigRestriction();
+    CheckConfigRestriction();
 
-	InitChCtlReg();
-	InitReqPool();
-	InitDependencyTable();
-	InitReqScheduler();
-	InitNandArray();
-	InitAddressMap();
-	InitDataBuf();
-	InitGcVictimMap();
+    InitChCtlReg();        // assigned the predefined addresses of channel controllers
+    InitReqPool();         //
+    InitDependencyTable(); //
+    InitReqScheduler();    //
+    InitNandArray();       // "[ NAND device reset complete. ]"
+    InitAddressMap();      // "Press 'X' to re-make the bad block table."
+    InitDataBuf();         //
+    InitGcVictimMap();     //
 
-	storageCapacity_L = (MB_PER_SSD - (MB_PER_MIN_FREE_BLOCK_SPACE + mbPerbadBlockSpace + MB_PER_OVER_PROVISION_BLOCK_SPACE)) * ((1024*1024) / BYTES_PER_NVME_BLOCK);
+    storageCapacity_L =
+        (MB_PER_SSD - (MB_PER_MIN_FREE_BLOCK_SPACE + mbPerbadBlockSpace + MB_PER_OVER_PROVISION_BLOCK_SPACE)) *
+        ((1024 * 1024) / BYTES_PER_NVME_BLOCK);
 
-	xil_printf("[ storage capacity %d MB ]\r\n", storageCapacity_L / ((1024*1024) / BYTES_PER_NVME_BLOCK));
-	xil_printf("[ ftl configuration complete. ]\r\n");
+    xil_printf("[ storage capacity %d MB ]\r\n", storageCapacity_L / ((1024 * 1024) / BYTES_PER_NVME_BLOCK));
+    xil_printf("[ ftl configuration complete. ]\r\n");
 }
 
-static void nfc_install_ucode(unsigned int* bram0)
+static void nfc_install_ucode(unsigned int *bram0)
 {
-	int i;
-	for (i = 0; i < T4NSCu_Common_CodeWordLength; i++)
-	{
-		bram0[i] = T4NSCuCode_Common[i];
-	}
-	for (i = 0; i < T4NSCu_PlainOps_CodeWordLength; i++)
-	{
-		bram0[T4NSCu_Common_CodeWordLength + i] = T4NSCuCode_PlainOps[i];
-	}
+    int i;
+    for (i = 0; i < T4NSCu_Common_CodeWordLength; i++)
+    {
+        bram0[i] = T4NSCuCode_Common[i];
+    }
+    for (i = 0; i < T4NSCu_PlainOps_CodeWordLength; i++)
+    {
+        bram0[T4NSCu_Common_CodeWordLength + i] = T4NSCuCode_PlainOps[i];
+    }
 }
 
+/* the mapped base addresses of NAND Storage Controllers */
 unsigned int NSCS[] = {
-	NSC_0_BASEADDR,
-	NSC_1_BASEADDR,
-	NSC_2_BASEADDR,
-	NSC_3_BASEADDR,
-	NSC_4_BASEADDR,
-	NSC_5_BASEADDR,
-	NSC_6_BASEADDR,
-	NSC_7_BASEADDR,
+    NSC_0_BASEADDR, NSC_1_BASEADDR, NSC_2_BASEADDR, NSC_3_BASEADDR,
+    NSC_4_BASEADDR, NSC_5_BASEADDR, NSC_6_BASEADDR, NSC_7_BASEADDR,
 };
 
-unsigned int NSC_UCODES[] = {
-	NSC_0_UCODEADDR,
-	NSC_1_UCODEADDR,
-	NSC_2_UCODEADDR,
-	NSC_3_UCODEADDR,
-	NSC_4_UCODEADDR,
-	NSC_5_UCODEADDR,
-	NSC_6_UCODEADDR,
-	NSC_7_UCODEADDR
-};
+unsigned int NSC_UCODES[] = {NSC_0_UCODEADDR, NSC_1_UCODEADDR, NSC_2_UCODEADDR, NSC_3_UCODEADDR,
+                             NSC_4_UCODEADDR, NSC_5_UCODEADDR, NSC_6_UCODEADDR, NSC_7_UCODEADDR};
 
+/**
+ * @brief Initialize the base addresses of all channel controllers.
+ */
 void InitChCtlReg()
 {
-	int i;
-	if(USER_CHANNELS < 1)
-		assert(!"[WARNING] Configuration Error: Channel [WARNING]");
+    int i;
+    if (USER_CHANNELS < 1)
+        assert(!"[WARNING] Configuration Error: Channel [WARNING]");
 
-	for (i = 0; i < USER_CHANNELS; i++)
-	{
-		nfc_install_ucode((unsigned int*)NSC_UCODES[i]);
-		V2FInitializeHandle(&chCtlReg[i], (void*)NSCS[i]);
-		nfc_set_dqs_delay(i, 28);
-	}
+    for (i = 0; i < USER_CHANNELS; i++)
+    {
+        nfc_install_ucode((unsigned int *)NSC_UCODES[i]);
+        V2FInitializeHandle(&chCtlReg[i], (void *)NSCS[i]);
+        nfc_set_dqs_delay(i, 28);
+    }
 
-	//nfc_set_dqs_delay(0, 31);
-	//nfc_set_dqs_delay(1, 31);
-	//nfc_set_dqs_delay(5, 4);
-	//nfc_set_dqs_delay(6, 18);
+    // nfc_set_dqs_delay(0, 31);
+    // nfc_set_dqs_delay(1, 31);
+    // nfc_set_dqs_delay(5, 4);
+    // nfc_set_dqs_delay(6, 18);
 }
 
+/**
+ * @brief Send RESET and SET_FEATURE to all the flash dies.
+ *
+ * This function send two requests to each die:
+ *
+ * 1. `REQ_CODE_RESET`:
+ *
+ *      //TODO
+ *
+ * 2. `REQ_CODE_SET_FEATURE`:
+ *
+ *      This request is used to make the flash enter the toggle mode, check the function
+ *      `IssueNandReq()` for details.
+ *
+ * The two types of request have some characteristics:
+ *
+ * - no need to do address translation -> use physical address directly:
+ *
+ *      set `nandAddr` to `REQ_OPT_NAND_ADDR_PHY_ORG`
+ *      set `physicalCh` to the channel number of target die
+ *      set `physicalWay` to the way number of target die
+ *      set `physicalBlock` to any value (no effect)
+ *      set `physicalPage` to any value (no effect)
+ *
+ * - no dependency problem:
+ *
+ *      set `rowAddrDependencyCheck` to `REQ_OPT_ROW_ADDR_DEPENDENCY_NONE`
+ *      set `prevBlockingReq` to `REQ_SLOT_TAG_NONE`
+ *
+ * - no need data buffer needed:
+ *
+ *      set `dataBufFormat` to `REQ_OPT_DATA_BUF_NONE`
+ *
+ * - apply to all the blocks (main space + extended space)
+ *
+ *      set `blockSpace` to `REQ_OPT_BLOCK_SPACE_TOTAL`
+ */
 void InitNandArray()
 {
-	unsigned int chNo, wayNo, reqSlotTag;
-	int i;
+    unsigned int chNo, wayNo, reqSlotTag;
+    int i;
 
-	for(chNo=0; chNo<USER_CHANNELS; ++chNo)
-		for(wayNo=0; wayNo<USER_WAYS; ++wayNo)
-		{
-			reqSlotTag = GetFromFreeReqQ();
+    for (chNo = 0; chNo < USER_CHANNELS; ++chNo)
+        for (wayNo = 0; wayNo < USER_WAYS; ++wayNo)
+        {
+            reqSlotTag                                                    = GetFromFreeReqQ();
+            reqPoolPtr->reqPool[reqSlotTag].reqType                       = REQ_TYPE_NAND;
+            reqPoolPtr->reqPool[reqSlotTag].reqCode                       = REQ_CODE_RESET;
+            reqPoolPtr->reqPool[reqSlotTag].reqOpt.nandAddr               = REQ_OPT_NAND_ADDR_PHY_ORG;
+            reqPoolPtr->reqPool[reqSlotTag].reqOpt.dataBufFormat          = REQ_OPT_DATA_BUF_NONE;
+            reqPoolPtr->reqPool[reqSlotTag].reqOpt.rowAddrDependencyCheck = REQ_OPT_ROW_ADDR_DEPENDENCY_NONE;
+            reqPoolPtr->reqPool[reqSlotTag].reqOpt.blockSpace             = REQ_OPT_BLOCK_SPACE_TOTAL;
+            reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalCh           = chNo;
+            reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalWay          = wayNo;
+            reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalBlock        = 0; // dummy
+            reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalPage         = 0; // dummy
+            reqPoolPtr->reqPool[reqSlotTag].prevBlockingReq               = REQ_SLOT_TAG_NONE;
+            SelectLowLevelReqQ(reqSlotTag);
 
-			reqPoolPtr->reqPool[reqSlotTag].reqType = REQ_TYPE_NAND;
-			reqPoolPtr->reqPool[reqSlotTag].reqCode = REQ_CODE_RESET;
-			reqPoolPtr->reqPool[reqSlotTag].reqOpt.nandAddr = REQ_OPT_NAND_ADDR_PHY_ORG;
-			reqPoolPtr->reqPool[reqSlotTag].reqOpt.dataBufFormat = REQ_OPT_DATA_BUF_NONE;
-			reqPoolPtr->reqPool[reqSlotTag].reqOpt.rowAddrDependencyCheck = REQ_OPT_ROW_ADDR_DEPENDENCY_NONE;
-			reqPoolPtr->reqPool[reqSlotTag].reqOpt.blockSpace = REQ_OPT_BLOCK_SPACE_TOTAL;
-			reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalCh = chNo;
-			reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalWay = wayNo;
-			reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalBlock = 0;	//dummy
-			reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalPage = 0;	//dummy
-			reqPoolPtr->reqPool[reqSlotTag].prevBlockingReq = REQ_SLOT_TAG_NONE;
+            reqSlotTag                                                    = GetFromFreeReqQ();
+            reqPoolPtr->reqPool[reqSlotTag].reqType                       = REQ_TYPE_NAND;
+            reqPoolPtr->reqPool[reqSlotTag].reqCode                       = REQ_CODE_SET_FEATURE;
+            reqPoolPtr->reqPool[reqSlotTag].reqOpt.nandAddr               = REQ_OPT_NAND_ADDR_PHY_ORG;
+            reqPoolPtr->reqPool[reqSlotTag].reqOpt.dataBufFormat          = REQ_OPT_DATA_BUF_NONE;
+            reqPoolPtr->reqPool[reqSlotTag].reqOpt.rowAddrDependencyCheck = REQ_OPT_ROW_ADDR_DEPENDENCY_NONE;
+            reqPoolPtr->reqPool[reqSlotTag].reqOpt.blockSpace             = REQ_OPT_BLOCK_SPACE_TOTAL;
+            reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalCh           = chNo;
+            reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalWay          = wayNo;
+            reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalBlock        = 0; // dummy
+            reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalPage         = 0; // dummy
+            reqPoolPtr->reqPool[reqSlotTag].prevBlockingReq               = REQ_SLOT_TAG_NONE;
+            SelectLowLevelReqQ(reqSlotTag);
+        }
 
-			SelectLowLevelReqQ(reqSlotTag);
+    /**
+     * These requests must be done in the initialization stage, make sure they are all
+     * done before going to next initialization step.
+     */
+    SyncAllLowLevelReqDone();
 
-			reqSlotTag = GetFromFreeReqQ();
+    for (i = 0; i < USER_CHANNELS; i++)
+    {
+        int j;
+        unsigned char *idData = (unsigned char *)(TEMPORARY_PAY_LOAD_ADDR + 16);
+        V2FReadIdSync(&chCtlReg[i], 0, idData);
+        printf("Ch %d ReadId: ", i);
+        for (j = 0; j < 6; j++)
+            printf("%x ", idData[j]);
+        printf("\r\n");
+    }
 
-			reqPoolPtr->reqPool[reqSlotTag].reqType = REQ_TYPE_NAND;
-			reqPoolPtr->reqPool[reqSlotTag].reqCode = REQ_CODE_SET_FEATURE;
-			reqPoolPtr->reqPool[reqSlotTag].reqOpt.nandAddr =  REQ_OPT_NAND_ADDR_PHY_ORG;
-			reqPoolPtr->reqPool[reqSlotTag].reqOpt.dataBufFormat = REQ_OPT_DATA_BUF_NONE;
-			reqPoolPtr->reqPool[reqSlotTag].reqOpt.rowAddrDependencyCheck = REQ_OPT_ROW_ADDR_DEPENDENCY_NONE;
-			reqPoolPtr->reqPool[reqSlotTag].reqOpt.blockSpace = REQ_OPT_BLOCK_SPACE_TOTAL;
-			reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalCh = chNo;
-			reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalWay = wayNo;
-			reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalBlock = 0;	//dummy
-			reqPoolPtr->reqPool[reqSlotTag].nandInfo.physicalPage = 0;	//dummy
-			reqPoolPtr->reqPool[reqSlotTag].prevBlockingReq = REQ_SLOT_TAG_NONE;
-
-			SelectLowLevelReqQ(reqSlotTag);
-		}
-
-	SyncAllLowLevelReqDone();
-
-	for (i = 0; i < USER_CHANNELS; i++)
-	{
-		int j;
-		unsigned char* idData = (unsigned char*)(TEMPORARY_PAY_LOAD_ADDR + 16);
-		V2FReadIdSync(&chCtlReg[i], 0, idData);
-		printf("Ch %d ReadId: ", i);
-		for (j = 0; j < 6;j ++)
-			printf("%x ", idData[j]);
-		printf("\r\n");
-	}
-
-	xil_printf("[ NAND device reset complete. ]\r\n");
+    xil_printf("[ NAND device reset complete. ]\r\n");
 }
 
-
+/**
+ * @brief Check the configurations are legal before the initializations start.
+ *
+ * This function will check two things:
+ *
+ * 1. the geometry of flash memory doesn't exceed the limit of flash controller
+ * 2. there is no overlap in the memory map
+ */
 void CheckConfigRestriction()
 {
-	if(USER_CHANNELS > NSC_MAX_CHANNELS)
-		assert(!"[WARNING] Configuration Error: Channel [WARNING]");
-	if(USER_WAYS > NSC_MAX_WAYS)
-		assert(!"[WARNING] Configuration Error: WAY [WARNING]");
-	if(USER_BLOCKS_PER_LUN > MAIN_BLOCKS_PER_LUN)
-		assert(!"[WARNING] Configuration Error: BLOCK [WARNING]");
-	if((BITS_PER_FLASH_CELL != SLC_MODE))
-		assert(!"[WARNING] Configuration Error: BIT_PER_FLASH_CELL [WARNING]");
+    // geometry configutations of flash memory
+    if (USER_CHANNELS > NSC_MAX_CHANNELS)
+        assert(!"[WARNING] Configuration Error: Channel [WARNING]");
+    if (USER_WAYS > NSC_MAX_WAYS)
+        assert(!"[WARNING] Configuration Error: WAY [WARNING]");
+    if (USER_BLOCKS_PER_LUN > MAIN_BLOCKS_PER_LUN)
+        assert(!"[WARNING] Configuration Error: BLOCK [WARNING]");
+    // @FIXME: only allow SLC mode ?
+    if ((BITS_PER_FLASH_CELL != SLC_MODE))
+        assert(!"[WARNING] Configuration Error: BIT_PER_FLASH_CELL [WARNING]");
 
-	if(RESERVED_DATA_BUFFER_BASE_ADDR + 0x00200000 > COMPLETE_FLAG_TABLE_ADDR)
-		assert(!"[WARNING] Configuration Error: Data buffer size is too large to be allocated to predefined range [WARNING]");
-	if(TEMPORARY_PAY_LOAD_ADDR + 0x00001000 > DATA_BUFFER_MAP_ADDR)
-		assert(!"[WARNING] Configuration Error: Metadata for NAND request completion process is too large to be allocated to predefined range [WARNING]");
-	if(FTL_MANAGEMENT_END_ADDR > DRAM_END_ADDR)
-		assert(!"[WARNING] Configuration Error: Metadata of FTL is too large to be allocated to DRAM [WARNING]");
+    // the size of some area are variable, make sure there is no overlap
+    if (RESERVED_DATA_BUFFER_BASE_ADDR + 0x00200000 > COMPLETE_FLAG_TABLE_ADDR)
+        assert(!"[WARNING] Configuration Error: Data buffer size is too large to be allocated to predefined range "
+                "[WARNING]");
+    if (TEMPORARY_PAY_LOAD_ADDR + 0x00001000 > DATA_BUFFER_MAP_ADDR)
+        assert(!"[WARNING] Configuration Error: Metadata for NAND request completion process is too large to be "
+                "allocated to predefined range [WARNING]");
+    if (FTL_MANAGEMENT_END_ADDR > DRAM_END_ADDR)
+        assert(!"[WARNING] Configuration Error: Metadata of FTL is too large to be allocated to DRAM [WARNING]");
 }
--- Toshiba-8c8w/ftl_config.h
+++ Toshiba-8c8w/ftl_config.h
@@ -51,189 +51,191 @@
 #include "xparameters.h"
 #include "nvme/nvme.h"
 
-//checks NSC connection, initializes base address
-#ifdef	XPAR_T4NFC_HLPER_7_BASEADDR
-#define NSC_7_CONNECTED	1
+/* -------------------------------------------------------------------------- */
+/*                   NAND Storage Controller Configurations                   */
+/* -------------------------------------------------------------------------- */
+
+#ifdef XPAR_T4NFC_HLPER_7_BASEADDR
+#define NSC_7_CONNECTED 1
 #define NSC_7_UCODEADDR XPAR_AXI_BRAM_CTRL_7_S_AXI_BASEADDR
-#define NSC_7_BASEADDR	XPAR_T4NFC_HLPER_7_BASEADDR
+#define NSC_7_BASEADDR  XPAR_T4NFC_HLPER_7_BASEADDR
 #else
-#define NSC_7_CONNECTED	0
+#define NSC_7_CONNECTED 0
 #define NSC_7_UCODEADDR 0
-#define NSC_7_BASEADDR	0
+#define NSC_7_BASEADDR  0
 #endif
-#ifdef	XPAR_T4NFC_HLPER_6_BASEADDR
-#define NSC_6_CONNECTED	1
+#ifdef XPAR_T4NFC_HLPER_6_BASEADDR
+#define NSC_6_CONNECTED 1
 #define NSC_6_UCODEADDR XPAR_AXI_BRAM_CTRL_6_S_AXI_BASEADDR
-#define NSC_6_BASEADDR	XPAR_T4NFC_HLPER_6_BASEADDR
+#define NSC_6_BASEADDR  XPAR_T4NFC_HLPER_6_BASEADDR
 #else
-#define NSC_6_CONNECTED	0
+#define NSC_6_CONNECTED 0
 #define NSC_6_UCODEADDR 0
-#define NSC_6_BASEADDR	0
+#define NSC_6_BASEADDR  0
 #endif
-#ifdef	XPAR_T4NFC_HLPER_5_BASEADDR
-#define NSC_5_CONNECTED	1
+#ifdef XPAR_T4NFC_HLPER_5_BASEADDR
+#define NSC_5_CONNECTED 1
 #define NSC_5_UCODEADDR XPAR_AXI_BRAM_CTRL_5_S_AXI_BASEADDR
-#define NSC_5_BASEADDR	XPAR_T4NFC_HLPER_5_BASEADDR
+#define NSC_5_BASEADDR  XPAR_T4NFC_HLPER_5_BASEADDR
 #else
-#define NSC_5_CONNECTED	0
+#define NSC_5_CONNECTED 0
 #define NSC_5_UCODEADDR 0
-#define NSC_5_BASEADDR	0
+#define NSC_5_BASEADDR  0
 #endif
-#ifdef	XPAR_T4NFC_HLPER_4_BASEADDR
-#define NSC_4_CONNECTED	1
+#ifdef XPAR_T4NFC_HLPER_4_BASEADDR
+#define NSC_4_CONNECTED 1
 #define NSC_4_UCODEADDR XPAR_AXI_BRAM_CTRL_4_S_AXI_BASEADDR
-#define NSC_4_BASEADDR	XPAR_T4NFC_HLPER_4_BASEADDR
+#define NSC_4_BASEADDR  XPAR_T4NFC_HLPER_4_BASEADDR
 #else
-#define NSC_4_CONNECTED	0
+#define NSC_4_CONNECTED 0
 #define NSC_4_UCODEADDR 0
-#define NSC_4_BASEADDR	0
+#define NSC_4_BASEADDR  0
 #endif
-#ifdef	XPAR_T4NFC_HLPER_3_BASEADDR
-#define NSC_3_CONNECTED	1
+#ifdef XPAR_T4NFC_HLPER_3_BASEADDR
+#define NSC_3_CONNECTED 1
 #define NSC_3_UCODEADDR XPAR_AXI_BRAM_CTRL_3_S_AXI_BASEADDR
-#define NSC_3_BASEADDR	XPAR_T4NFC_HLPER_3_BASEADDR
+#define NSC_3_BASEADDR  XPAR_T4NFC_HLPER_3_BASEADDR
 #else
-#define NSC_3_CONNECTED	0
+#define NSC_3_CONNECTED 0
 #define NSC_3_UCODEADDR 0
-#define NSC_3_BASEADDR	0
+#define NSC_3_BASEADDR  0
 #endif
-#ifdef	XPAR_T4NFC_HLPER_2_BASEADDR
-#define NSC_2_CONNECTED	1
+#ifdef XPAR_T4NFC_HLPER_2_BASEADDR
+#define NSC_2_CONNECTED 1
 #define NSC_2_UCODEADDR XPAR_AXI_BRAM_CTRL_2_S_AXI_BASEADDR
-#define NSC_2_BASEADDR	XPAR_T4NFC_HLPER_2_BASEADDR
+#define NSC_2_BASEADDR  XPAR_T4NFC_HLPER_2_BASEADDR
 #else
-#define NSC_2_CONNECTED	0
+#define NSC_2_CONNECTED 0
 #define NSC_2_UCODEADDR 0
-#define NSC_2_BASEADDR	0
+#define NSC_2_BASEADDR  0
 #endif
-#ifdef	XPAR_T4NFC_HLPER_1_BASEADDR
-#define NSC_1_CONNECTED	1
+#ifdef XPAR_T4NFC_HLPER_1_BASEADDR
+#define NSC_1_CONNECTED 1
 #define NSC_1_UCODEADDR XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR
-#define NSC_1_BASEADDR	XPAR_T4NFC_HLPER_1_BASEADDR
+#define NSC_1_BASEADDR  XPAR_T4NFC_HLPER_1_BASEADDR
 #else
-#define NSC_1_CONNECTED	0
+#define NSC_1_CONNECTED 0
 #define NSC_1_UCODEADDR 0
-#define NSC_1_BASEADDR	0
+#define NSC_1_BASEADDR  0
 #endif
-#ifdef	XPAR_T4NFC_HLPER_0_BASEADDR
-#define NSC_0_CONNECTED	1
+#ifdef XPAR_T4NFC_HLPER_0_BASEADDR
+#define NSC_0_CONNECTED 1
 #define NSC_0_UCODEADDR XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR
-#define NSC_0_BASEADDR	XPAR_T4NFC_HLPER_0_BASEADDR
+#define NSC_0_BASEADDR  XPAR_T4NFC_HLPER_0_BASEADDR
 #else
-#define NSC_0_CONNECTED	0
+#define NSC_0_CONNECTED 0
 #define NSC_0_UCODEADDR 0
-#define NSC_0_BASEADDR	0
+#define NSC_0_BASEADDR  0
 #endif
 
-//number of connected (=AXI mapped) NSC
-#define NUMBER_OF_CONNECTED_CHANNEL (NSC_7_CONNECTED + NSC_6_CONNECTED + NSC_5_CONNECTED + NSC_4_CONNECTED + NSC_3_CONNECTED + NSC_2_CONNECTED + NSC_1_CONNECTED + NSC_0_CONNECTED)
+// number of connected (=AXI mapped) NSC
+#define NUMBER_OF_CONNECTED_CHANNEL                                                                               \
+    (NSC_7_CONNECTED + NSC_6_CONNECTED + NSC_5_CONNECTED + NSC_4_CONNECTED + NSC_3_CONNECTED + NSC_2_CONNECTED +  \
+     NSC_1_CONNECTED + NSC_0_CONNECTED)
 
+/* -------------------------------------------------------------------------- */
+/*                      NAND flash memory specifications                      */
+/* -------------------------------------------------------------------------- */
 
-//--------------------------------
-//NAND flash memory specifications
-//--------------------------------
+#define BYTES_PER_DATA_REGION_OF_NAND_ROW  16384 /* the size of data region of a page */
+#define BYTES_PER_SPARE_REGION_OF_NAND_ROW 1664  /* the size of spare region of a page */
 
-#define	BYTES_PER_DATA_REGION_OF_NAND_ROW		16384
-#define	BYTES_PER_SPARE_REGION_OF_NAND_ROW		1664
-#define BYTES_PER_NAND_ROW						(BYTES_PER_DATA_REGION_OF_NAND_ROW + BYTES_PER_SPARE_REGION_OF_NAND_ROW)
+/* the size in bytes of a whole flash page (data + metadata + ECC). */
+#define BYTES_PER_NAND_ROW (BYTES_PER_DATA_REGION_OF_NAND_ROW + BYTES_PER_SPARE_REGION_OF_NAND_ROW)
 
-#define	ROWS_PER_SLC_BLOCK			256
-#define	ROWS_PER_MLC_BLOCK			256
+// @FIXME: why SLC mode == MLC mode ?
+#define ROWS_PER_SLC_BLOCK 256 /* number of page in this block (SLC mode) */
+#define ROWS_PER_MLC_BLOCK 256 /* number of page in this block (MLC mode) */
 
-#define	MAIN_BLOCKS_PER_LUN			2732
-#define EXTENDED_BLOCKS_PER_LUN		224
-#define TOTAL_BLOCKS_PER_LUN		(MAIN_BLOCKS_PER_LUN + EXTENDED_BLOCKS_PER_LUN)
+#define MAIN_BLOCKS_PER_LUN     2732 /* number of blocks in the main space of a die */
+#define EXTENDED_BLOCKS_PER_LUN 224  /* number of blocks in the extended space of a die */
+#define TOTAL_BLOCKS_PER_LUN    (MAIN_BLOCKS_PER_LUN + EXTENDED_BLOCKS_PER_LUN)
 
-#define	MAIN_ROWS_PER_SLC_LUN		(ROWS_PER_SLC_BLOCK * MAIN_BLOCKS_PER_LUN)
-#define	MAIN_ROWS_PER_MLC_LUN		(ROWS_PER_MLC_BLOCK * MAIN_BLOCKS_PER_LUN)
+#define MAIN_ROWS_PER_SLC_LUN (ROWS_PER_SLC_BLOCK * MAIN_BLOCKS_PER_LUN)
+#define MAIN_ROWS_PER_MLC_LUN (ROWS_PER_MLC_BLOCK * MAIN_BLOCKS_PER_LUN)
 
-#define	LUNS_PER_DIE				1
+#define LUNS_PER_DIE 1 /* number of planes in a die (way) */
 
-#define	MAIN_BLOCKS_PER_DIE			(MAIN_BLOCKS_PER_LUN * LUNS_PER_DIE)
-#define TOTAL_BLOCKS_PER_DIE		(TOTAL_BLOCKS_PER_LUN * LUNS_PER_DIE)
+#define MAIN_BLOCKS_PER_DIE  (MAIN_BLOCKS_PER_LUN * LUNS_PER_DIE)
+#define TOTAL_BLOCKS_PER_DIE (TOTAL_BLOCKS_PER_LUN * LUNS_PER_DIE)
 
-#define BAD_BLOCK_MARK_PAGE0		0										//first row of a block
-#define BAD_BLOCK_MARK_PAGE1		(ROWS_PER_MLC_BLOCK - 1)				//last row of a block
-#define BAD_BLOCK_MARK_BYTE0 		0										//first byte of data region of the row
-#define BAD_BLOCK_MARK_BYTE1 		(BYTES_PER_DATA_REGION_OF_NAND_ROW)		//first byte of spare region of the row
-
-
-
+#define BAD_BLOCK_MARK_PAGE0 0                                   // first row of a block
+#define BAD_BLOCK_MARK_PAGE1 (ROWS_PER_MLC_BLOCK - 1)            // last row of a block
+#define BAD_BLOCK_MARK_BYTE0 0                                   // first byte of data region of the row
+#define BAD_BLOCK_MARK_BYTE1 (BYTES_PER_DATA_REGION_OF_NAND_ROW) // first byte of spare region of the row
 
 //------------------------------------
-//NAND storage controller specifications
+// NAND storage controller specifications
 //------------------------------------
 
-//supported maximum channel/way structure
-#define	NSC_MAX_CHANNELS				(NUMBER_OF_CONNECTED_CHANNEL)
-#define	NSC_MAX_WAYS					8
+// supported maximum channel/way structure
+#define NSC_MAX_CHANNELS (NUMBER_OF_CONNECTED_CHANNEL)
+#define NSC_MAX_WAYS     8
 
-//row -> page
-#define	BYTES_PER_DATA_REGION_OF_PAGE			16384
-#define BYTES_PER_SPARE_REGION_OF_PAGE			256
+// row -> page
+#define BYTES_PER_DATA_REGION_OF_PAGE  16384
+#define BYTES_PER_SPARE_REGION_OF_PAGE 256 // last 8 byte used by ECC engine (CRC function)
 // (BYTES_PER_SPARE_REGION_OF_NAND_ROW - BYTES_PER_SPARE_REGION_OF_PAGE) bytes are used by ECC engine (Parity data)
-#define	PAGES_PER_SLC_BLOCK			(ROWS_PER_SLC_BLOCK)
-#define	PAGES_PER_MLC_BLOCK			(ROWS_PER_MLC_BLOCK)
+#define PAGES_PER_SLC_BLOCK (ROWS_PER_SLC_BLOCK)
+#define PAGES_PER_MLC_BLOCK (ROWS_PER_MLC_BLOCK)
 
-//ECC encoder/decoder specification
-#define ECC_CHUNKS_PER_PAGE				32
-#define BIT_ERROR_THRESHOLD_PER_CHUNK	24
-#define ERROR_INFO_WORD_COUNT 			11
-
+// ECC encoder/decoder specification
+#define ECC_CHUNKS_PER_PAGE           32
+#define BIT_ERROR_THRESHOLD_PER_CHUNK 24
+#define ERROR_INFO_WORD_COUNT         11
 
 //------------------------------
-//NVMe Controller Specifications
+// NVMe Controller Specifications
 //------------------------------
 
-#define	BYTES_PER_NVME_BLOCK		4096
-#define NVME_BLOCKS_PER_PAGE		(BYTES_PER_DATA_REGION_OF_PAGE / BYTES_PER_NVME_BLOCK)
-
-
+#define BYTES_PER_NVME_BLOCK 4096
+#define NVME_BLOCKS_PER_PAGE (BYTES_PER_DATA_REGION_OF_PAGE / BYTES_PER_NVME_BLOCK)
 
 //------------------
-//FTL configurations
+// FTL configurations
 //------------------
 
-#define	SLC_MODE				1
-#define	MLC_MODE				2
+#define SLC_MODE 1
+#define MLC_MODE 2
 
 //************************************************************************
-#define	BITS_PER_FLASH_CELL		SLC_MODE	//user configurable factor
-#define	USER_BLOCKS_PER_LUN		2048		//user configurable factor
-#define	USER_CHANNELS		(NUMBER_OF_CONNECTED_CHANNEL)		//user configurable factor
-#define	USER_WAYS				8			//user configurable factor
+#define BITS_PER_FLASH_CELL SLC_MODE // user configurable factor
+#define USER_BLOCKS_PER_LUN 2048     // user configurable factor
+#define USER_CHANNELS       8        // user configurable factor
+#define USER_WAYS           8        // user configurable factor
 //************************************************************************
 
-#define	BYTES_PER_DATA_REGION_OF_SLICE		16384		//slice is a mapping unit of FTL
-#define	BYTES_PER_SPARE_REGION_OF_SLICE		256
+// slice size, the mapping unit of FTL, equal to page size
+#define BYTES_PER_DATA_REGION_OF_SLICE  16384
+#define BYTES_PER_SPARE_REGION_OF_SLICE 256
 
-#define SLICES_PER_PAGE				(BYTES_PER_DATA_REGION_OF_PAGE / BYTES_PER_DATA_REGION_OF_SLICE)	//a slice directs a page, full page mapping
-#define NVME_BLOCKS_PER_SLICE		(BYTES_PER_DATA_REGION_OF_SLICE / BYTES_PER_NVME_BLOCK)
+#define SLICES_PER_PAGE                                                                                           \
+    (BYTES_PER_DATA_REGION_OF_PAGE / BYTES_PER_DATA_REGION_OF_SLICE) // a slice directs a page, full page mapping
+#define NVME_BLOCKS_PER_SLICE (BYTES_PER_DATA_REGION_OF_SLICE / BYTES_PER_NVME_BLOCK)
 
-#define	USER_DIES					(USER_CHANNELS * USER_WAYS)
+#define USER_DIES (USER_CHANNELS * USER_WAYS)
 
-#define	USER_PAGES_PER_BLOCK		(PAGES_PER_SLC_BLOCK * BITS_PER_FLASH_CELL)
-#define	USER_PAGES_PER_LUN			(USER_PAGES_PER_BLOCK * USER_BLOCKS_PER_LUN)
-#define	USER_PAGES_PER_DIE			(USER_PAGES_PER_LUN * LUNS_PER_DIE)
-#define	USER_PAGES_PER_CHANNEL		(USER_PAGES_PER_DIE * USER_WAYS)
-#define	USER_PAGES_PER_SSD			(USER_PAGES_PER_CHANNEL * USER_CHANNELS)
+#define USER_PAGES_PER_BLOCK   (PAGES_PER_SLC_BLOCK * BITS_PER_FLASH_CELL)
+#define USER_PAGES_PER_LUN     (USER_PAGES_PER_BLOCK * USER_BLOCKS_PER_LUN)
+#define USER_PAGES_PER_DIE     (USER_PAGES_PER_LUN * LUNS_PER_DIE)
+#define USER_PAGES_PER_CHANNEL (USER_PAGES_PER_DIE * USER_WAYS)
+#define USER_PAGES_PER_SSD     (USER_PAGES_PER_CHANNEL * USER_CHANNELS)
 
-#define	SLICES_PER_BLOCK			(USER_PAGES_PER_BLOCK * SLICES_PER_PAGE)
-#define	SLICES_PER_LUN				(USER_PAGES_PER_LUN * SLICES_PER_PAGE)
-#define	SLICES_PER_DIE				(USER_PAGES_PER_DIE * SLICES_PER_PAGE)
-#define	SLICES_PER_CHANNEL			(USER_PAGES_PER_CHANNEL * SLICES_PER_PAGE)
-#define	SLICES_PER_SSD				(USER_PAGES_PER_SSD * SLICES_PER_PAGE)
+#define SLICES_PER_BLOCK   (USER_PAGES_PER_BLOCK * SLICES_PER_PAGE)
+#define SLICES_PER_LUN     (USER_PAGES_PER_LUN * SLICES_PER_PAGE)
+#define SLICES_PER_DIE     (USER_PAGES_PER_DIE * SLICES_PER_PAGE)
+#define SLICES_PER_CHANNEL (USER_PAGES_PER_CHANNEL * SLICES_PER_PAGE)
+#define SLICES_PER_SSD     (USER_PAGES_PER_SSD * SLICES_PER_PAGE)
 
-#define	USER_BLOCKS_PER_DIE			(USER_BLOCKS_PER_LUN * LUNS_PER_DIE)
-#define	USER_BLOCKS_PER_CHANNEL		(USER_BLOCKS_PER_DIE * USER_WAYS)
-#define	USER_BLOCKS_PER_SSD			(USER_BLOCKS_PER_CHANNEL * USER_CHANNELS)
+#define USER_BLOCKS_PER_DIE     (USER_BLOCKS_PER_LUN * LUNS_PER_DIE)
+#define USER_BLOCKS_PER_CHANNEL (USER_BLOCKS_PER_DIE * USER_WAYS)
+#define USER_BLOCKS_PER_SSD     (USER_BLOCKS_PER_CHANNEL * USER_CHANNELS)
 
-#define	MB_PER_BLOCK						((BYTES_PER_DATA_REGION_OF_SLICE * SLICES_PER_BLOCK) / (1024 * 1024))
-#define MB_PER_SSD							(USER_BLOCKS_PER_SSD * MB_PER_BLOCK)
-#define MB_PER_MIN_FREE_BLOCK_SPACE			(USER_DIES * MB_PER_BLOCK)
-#define MB_PER_METADATA_BLOCK_SPACE			(USER_DIES * MB_PER_BLOCK)
-#define MB_PER_OVER_PROVISION_BLOCK_SPACE	((USER_BLOCKS_PER_SSD / 10) * MB_PER_BLOCK)
-
+#define MB_PER_BLOCK                      ((BYTES_PER_DATA_REGION_OF_SLICE * SLICES_PER_BLOCK) / (1024 * 1024))
+#define MB_PER_SSD                        (USER_BLOCKS_PER_SSD * MB_PER_BLOCK)
+#define MB_PER_MIN_FREE_BLOCK_SPACE       (USER_DIES * MB_PER_BLOCK)
+#define MB_PER_METADATA_BLOCK_SPACE       (USER_DIES * MB_PER_BLOCK)
+#define MB_PER_OVER_PROVISION_BLOCK_SPACE ((USER_BLOCKS_PER_SSD / 10) * MB_PER_BLOCK)
 
 void InitFTL();
 void InitChCtlReg();
